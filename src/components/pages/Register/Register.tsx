529266c3e0f89ff94486c0612bff54cd589581b9
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-08-26 16:05:49 +0200

    Updated Register.tsx to save the entered details on sign up.

--- src/components/pages/Register/Register.tsx
+++ src/components/pages/Register/Register.tsx
@@ -6,29 +6,42 @@ import { TextInput } from '../../inputs/TextInput'
 import { useNavigate } from 'react-router'
 import { CheckboxInput } from '../../inputs/CheckboxInput'
 import { useState } from 'react'
+import { useAppDispatch } from '../../../store/hooks'
+import { updateProfile } from '../../../store/profileSlice'
 
 export const Register = () => {
-    const [agreeToTerms, setAgreeToTerms] = useState<boolean>(false);
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [cellNumber, setCellNumber] = useState('')
+    const [agreeToTerms, setAgreeToTerms] = useState<boolean>(false)
+
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
 
     const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAgreeToTerms(e.target.checked);
+        setAgreeToTerms(e.target.checked)
     }
 
-    const navigate = useNavigate();
+    const handleSubmit = async () => {
+        if (!agreeToTerms) return
+        await dispatch(updateProfile({ name, surname, cellNumber }))
+        navigate('/Home')
+    }
+
     return (
         <div className='register-page'>
             <img src={backArrow} alt='Back Arrow Icon' id='back-arrow' onClick={() => navigate('/')} />
             <TextComponent variant='h3'>WELCOME</TextComponent>
             <TextComponent variant='h1'>Sign Up</TextComponent>
             <div className='register-fields'>
-                <TextInput label='Name' id='name' placeholder='eg. Mark' type='text' ></TextInput>
-                <TextInput label='Surname' id='surname' placeholder='eg. Johnson' type='text'></TextInput>
-                <TextInput label='Email' id='email' placeholder='markjohnson@example.com' type='email'></TextInput>
-                <TextInput label='Password' id='password' placeholder='*********' type='password'></TextInput>
-                <TextInput label='Cell Number' id='cellNumber' placeholder='(+27) 123 456 789' type='cellNumber'></TextInput>
+                <TextInput label='Name' id='name' placeholder='eg. Mark' type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                <TextInput label='Surname' id='surname' placeholder='eg. Johnson' type='text' value={surname} onChange={(e) => setSurname(e.target.value)} />
+                <TextInput label='Email' id='email' placeholder='markjohnson@example.com' type='email' />
+                <TextInput label='Password' id='password' placeholder='*********' type='password' />
+                <TextInput label='Cell Number' id='cellNumber' placeholder='(+27) 123 456 789' type='text' value={cellNumber} onChange={(e) => setCellNumber(e.target.value)} />
 
                 <CheckboxInput id='agree-checkbox' name='terms' checked={agreeToTerms} onChange={handleCheck} label={' I agree to the Terms & Conditions.'} />
-                <ButtonComponent onClick={() => navigate('/Home')} className='button-2'>Sign Up</ButtonComponent>
+                <ButtonComponent onClick={handleSubmit} className='button-2'>Sign Up</ButtonComponent>
             </div>
         </div>
     )
